3afc453c54e0d60e3f7563864f2fdc8fd83c0b52
Magery <rishab3447@gmail.com>
2022-05-07 17:45:49 +0530

    Commit 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
+import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
+
 let navLinks = document.getElementById("mobile-menu")
 let showBtn = document.getElementById("show")
 let hideBtn = document.getElementById("hide")
+
+const swiper = new Swiper('.swiper', {
+				autoplay: {
+					delay: 5000,
+					disableOnInteraction: true,
+				},
+				loop: true,
+				pagination: {
+					el: '.swiper-pagination',
+					clickable: true,
+				},
+
+				navigation: {
+					nextEl: '.swiper-button-next',
+					prevEl: '.swiper-button-prev',
+				},
+			})
 
 function showMenu(){
   navLinks.classList.remove('hidden');
@@ -13,3 +32,4 @@ function hideMenu(){
   showBtn.classList.remove('hidden');
   hideBtn.classList.add('hidden');
 }
+
